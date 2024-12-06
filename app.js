f54ce93f96a5c3e327a36349b87e475ef1aaf817
sameh <samehbouzid20@outlook.com>
2024-12-06 20:55:42 +0100

    Reinstall dependencies

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
+
 const dotenv = require('dotenv');
 const path = require('path');
 const multer = require('multer');
